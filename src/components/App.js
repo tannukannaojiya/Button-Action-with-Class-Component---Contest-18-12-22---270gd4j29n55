b1de16a5af6642b52ae89d43a5491dd39c1267b7
tannukannaojiya <tannukannaojiya@gmail.com>
2022-12-18 19:21:50 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,11 @@ class App extends Component {
     	return(
     		<div id="main">
 				{ /* Do not remove this main div!! */ }
+
+				{
+					(displayMassage === false)?(null):(<p id="para">Helllo, I've learnt to use the full-stack evalution tool.This makes me so happy</p>)
+				}
+				<button id="click" onClick={()=>setDisplayMassage(true)}>Click Me</button>
     		</div>
     	);
     }
